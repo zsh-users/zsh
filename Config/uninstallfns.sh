8e5365152aa58bb0d8a209efe176e1e7ae70675f
Tanaka Akira <akr@users.sourceforge.net>
1999-08-31 10:23:46 +0000

    manual/7562

--- Config/uninstallfns.sh
+++ Config/uninstallfns.sh
@@ -7,22 +7,22 @@ for file in $FUNCTIONS_INSTALL; do
   if test -f "$sdir/$file"; then
     install="$install $file"
   else
-    install="$install `echo $sdir/$file | sed -e \"s%$sdir/%%g\"`"
+    install="$install `echo '' $sdir/$file | sed -e \"s% $sdir/%%g\"`"
   fi
 done
 
 for file in $install; do
   if test -f $sdir/$file; then
     if test x$FUNCTIONS_SUBDIRS != x -a x$FUNCTIONS_SUBDIRS != xno; then
-      rm -f $fndir/$file;
-      if test -f $fndir.old/$file; then
-	mv $fndir.old/$file $fndir/$file
+      rm -f $DESTDIR$fndir/$file;
+      if test -f $DESTDIR$fndir.old/$file; then
+	mv $DESTDIR$fndir.old/$file $DESTDIR$fndir/$file
       fi
     else
       bfile="`echo $file | sed -e 's%^.*/%%'`"
-      rm -f "$fndir/$bfile"; \
-      if test -f $fndir.old/$bfile; then
-        mv $fndir.old/$bfile $fndir/$bfile
+      rm -f "$DESTDIR$fndir/$bfile"; \
+      if test -f $DESTDIR$fndir.old/$bfile; then
+        mv $DESTDIR$fndir.old/$bfile $DESTDIR$fndir/$bfile
       fi
     fi
   fi
