d0974ede0b80cec97b88d8928ba1b664a312a001
Tanaka Akira <akr@users.sourceforge.net>
1999-12-08 22:05:45 +0000

    zsh-3.1.6-pws-11

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=3.1.6-pws-10
-VERSION_DATE='November 29, 1999'
+VERSION=3.1.6-pws-11
+VERSION_DATE='December 8, 1999'
