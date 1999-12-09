d0c0f1a95cb406c71a434bf6a006e9f358b4d24c
Tanaka Akira <akr@users.sourceforge.net>
1999-12-09 11:01:30 +0000

    zsh-workers/8968

--- Config/config.mk
+++ Config/config.mk
@@ -36,3 +36,7 @@ $(dir_top)/Config/defs.mk $(dir_top)/Config/version.mk
 Makefile: Makefile.in $(dir_top)/config.status $(CONFIG_INCS)
 	cd $(dir_top) && \
 	  CONFIG_FILES=$(subdir)/$@ CONFIG_HEADERS= ./config.status
+
+$(dir_top)/Config/defs.mk: $(sdir_top)/Config/defs.mk.in
+	cd $(dir_top) && \
+	  CONFIG_FILES=Config/defs.mk CONFIG_HEADERS= ./config.status
