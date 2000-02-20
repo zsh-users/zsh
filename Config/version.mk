6ad3885077846037c63b152030f289a4d7a94980
Tanaka Akira <akr@users.sourceforge.net>
2000-02-20 19:16:52 +0000

    zsh-3.1.6-dev-19

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=3.1.6-dev-18
-VERSION_DATE='February 11, 2000'
+VERSION=3.1.6-dev-19
+VERSION_DATE='February 20, 2000'
