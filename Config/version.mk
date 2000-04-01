e025336f2f6d9f107ee1e03b9900f04af0544ba9
Peter Stephenson <pws@users.sourceforge.net>
2000-04-01 20:43:43 +0000

    Updated from list as far as 10376

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=3.1.5
-VERSION_DATE='October 29, 1998'
+VERSION=3.1.6-dev-21
+VERSION_DATE='April 1, 2000'
