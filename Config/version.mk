1b15c688dc9cd826e8df91fe9d2d8e09605e8a66
Peter Stephenson <pws@users.sourceforge.net>
2000-06-19 09:13:44 +0000

    users/3189: zmv fixes Version 3.1.9-dev-1

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=3.1.9
-VERSION_DATE='June 5, 2000'
+VERSION=3.1.9-dev-1
+VERSION_DATE='June 19, 2000'
