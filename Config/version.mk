4f9ff8206ba046343c77bb7698c2e66cc5b4edf2
Peter Stephenson <pws@users.sourceforge.net>
2000-07-31 09:34:37 +0000

    Add compaudit to .distfiles; 3.1.9-dev-4

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=3.1.9-dev-3
-VERSION_DATE='July 17, 2000'
+VERSION=3.1.9-dev-4
+VERSION_DATE='July 31, 2000'
