dadb12c674c1609d2966d6c39685dd51f9c0dbcf
Peter Stephenson <pws@users.sourceforge.net>
2000-12-05 14:31:50 +0000

    3.1.9-dev-8

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=3.1.9-dev-7
-VERSION_DATE='October 17, 2000'
+VERSION=3.1.9-dev-8
+VERSION_DATE='December 5, 2000'
