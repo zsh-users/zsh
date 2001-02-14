5d60b669447a7229cd96bdbcdb1573cc04fe89a9
Peter Stephenson <pws@users.sourceforge.net>
2001-02-14 11:53:30 +0000

    Updated to 4.0.1-pre-1

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=3.1.9-dev-8
-VERSION_DATE='December 5, 2000'
+VERSION=4.0.1-pre-1
+VERSION_DATE='February 14, 2001'
