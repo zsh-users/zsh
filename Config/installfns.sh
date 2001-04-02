956ef612e2a797a3f0325cafb034b654b9e8922f
Sven Wischnowsky <wischnow@users.sourceforge.net>
2001-04-02 14:36:36 +0000

    cleanup after function move

--- Config/installfns.sh
+++ Config/installfns.sh
@@ -15,6 +15,10 @@ for file in $allfuncs; do
   if test -f $sdir_top/$file; then
     if test x$FUNCTIONS_SUBDIRS != x -a x$FUNCTIONS_SUBDIRS != xno; then
       case "$file" in
+      Completion/comp*)
+        subdir="`echo $file | sed -e 's%/[^/]*/[^/]*$%%'`"
+        instdir="$fndir/Completion"
+        ;;
       Completion/*)
         subdir="`echo $file | sed -e 's%/[^/]*/[^/]*$%%'`"
         instdir="$fndir/$subdir"
