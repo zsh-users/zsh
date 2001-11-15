4a15ed83bc367df0c1e03bd63b3e425f0000b7f0
Peter Stephenson <pws@users.sourceforge.net>
2001-11-15 18:44:45 +0000

    unposted: version 4.1.0-dev-3 with associated .distfiles tidying

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=4.1.0-dev-2
-VERSION_DATE='September 27, 2001'
+VERSION=4.1.0-dev-3
+VERSION_DATE='November 15, 2001'
