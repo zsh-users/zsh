b9f675c0e09a5727887f74b8c1dafc36dfce0796
Oliver Kiddle <opk@users.sourceforge.net>
2001-12-18 14:51:30 +0000

    16364: further to 16351, except specials from having their value cleared

--- Src/params.c
+++ Src/params.c
@@ -2286,7 +2286,10 @@ stdunsetfn(Param pm, int exp)
 	case PM_SCALAR: pm->sets.cfn(pm, NULL); break;
 	case PM_ARRAY:  pm->sets.afn(pm, NULL); break;
 	case PM_HASHED: pm->sets.hfn(pm, NULL); break;
-	default: pm->u.str = NULL; break;
+	default:
+	    if (!(pm->flags & PM_SPECIAL))
+	    	pm->u.str = NULL;
+	    break;
     }
     pm->flags |= PM_UNSET;
 }
