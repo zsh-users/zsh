d367f4c62c68cbbe1b64895c3ad18227c6bcb371
Sven Wischnowsky <wischnow@users.sourceforge.net>
2002-04-25 07:34:48 +0000

    17035: remove one half of 17029; the part for zstyle wasn't right

--- Src/Modules/zutil.c
+++ Src/Modules/zutil.c
@@ -132,7 +132,6 @@ setstypat(Style s, char *pat, Patprog prog, char **vals, int eval)
 		freearray(p->vals);
 	    if (p->eval)
 		freeeprog(p->eval);
-            freepatprog(p->prog);
 	    p->vals = zarrdup(vals);
 	    p->eval = eprog;
 
