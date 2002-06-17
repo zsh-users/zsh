f15a9108cf9ef3bf64c2c54bef1fe7e989f83fe9
Peter Stephenson <pws@users.sourceforge.net>
2002-06-17 13:15:44 +0000

    Version 4.1.0-dev-5

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=4.1.0-dev-4
-VERSION_DATE='March 5, 2002'
+VERSION=4.1.0-dev-5
+VERSION_DATE='June 17, 2002'
