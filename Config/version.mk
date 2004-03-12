b74e003f9cf9ae541869b3f6aab4a86a056c6cca
Peter Stephenson <pws@users.sourceforge.net>
2004-03-12 16:38:47 +0000

    19615: update MACHINES

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=4.2.0-pre-3
-VERSION_DATE='March 5, 2004'
+VERSION=4.2.0-pre-4
+VERSION_DATE='March 12, 2004'
