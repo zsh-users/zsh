4075f654007cd85e4edd9e5bd87b6bd9eeac763b
Peter Stephenson <pws@users.sourceforge.net>
2005-12-12 18:51:47 +0000

    Missed bit from 4.3.0-dev-2

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=4.3.0-dev-1
-VERSION_DATE='February 7, 2005'
+VERSION=4.3.0-dev-2
+VERSION_DATE='December 9, 2005'
