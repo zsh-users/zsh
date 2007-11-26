8d22b3a4f8551f47fd96eb4e438005ec2a40d7d6
Peter Stephenson <pws@users.sourceforge.net>
2007-11-26 13:48:44 +0000

    unposted: changes for 4.3.4-dev-3

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=4.3.4-dev-2
-VERSION_DATE='November 20, 2007'
+VERSION=4.3.4-dev-3
+VERSION_DATE='November 26, 2007'
