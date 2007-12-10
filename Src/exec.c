4736e253beca45aed88ef9a8d5225c7164208c45
Paul Ackersviller <packersv@users.sourceforge.net>
2007-12-10 03:16:53 +0000

    Merge 24150: Exit status of null command should be exit status of last cmdsubst.

--- Src/exec.c
+++ Src/exec.c
@@ -1963,7 +1963,7 @@ execcmd(Estate state, int input, int output, int how, int last1)
 		    lastval = 0;
 		    return;
 		} else {
-		    cmdoutval = 0;
+		    cmdoutval = lastval;
 		    if (varspc)
 			addvars(state, varspc, 0);
 		    if (errflag)
