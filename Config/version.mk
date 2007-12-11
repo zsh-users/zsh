11dc3a8cd790753a6da5ae08a23b3cef2041f2d1
Peter Stephenson <pws@users.sourceforge.net>
2007-12-11 10:09:49 +0000

    unposted: 4.2.6-dev-2

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=4.2.6
-VERSION_DATE='November 28, 2005'
+VERSION=4.2.6-dev-2
+VERSION_DATE='December 11, 2007'
