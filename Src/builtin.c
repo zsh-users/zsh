172a81db90b9b5a499fb10f1b6334dee32b24d88
Wayne Davison <wayned@users.sourceforge.net>
2008-01-21 19:15:39 +0000

    Silence a compiler warning.

--- Src/builtin.c
+++ Src/builtin.c
@@ -4970,7 +4970,7 @@ bin_read(char *name, char **args, Options ops, UNUSED(int func))
 			    }
 			    else if (ret == 0) /* handle null as normal char */
 				ret = 1;
-			    else if (ret > val) {
+			    else if (ret > (size_t)val) {
 				/* Some mbrlen()s return the full char len */
 				ret = val;
 			    }
