20ec72c83bb598e9615a137884455925a343a685
Peter Stephenson <pws@users.sourceforge.net>
2008-01-29 14:29:19 +0000

    4.3.4-dev-8

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=4.3.4-dev-7
-VERSION_DATE='January 8, 2008'
+VERSION=4.3.4-dev-8
+VERSION_DATE='January 29, 2008'
