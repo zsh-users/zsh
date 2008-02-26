3934b6d62123e0b1a310fbc478ce45a1913f76fc
Geoff Wing <gcw@users.sourceforge.net>
2008-02-26 17:53:31 +0000

    another fatfinger: change comparison to asignment

--- Src/utils.c
+++ Src/utils.c
@@ -4887,7 +4887,7 @@ getkeystring(char *s, int *len, int how, int *misc)
 		     */
 		    if (!codesetstr || !*codesetstr ||
 			!strcmp(codesetstr, "646"))
-			codesetstr == "US-ASCII";
+			codesetstr = "US-ASCII";
     	    	    cd = iconv_open(nl_langinfo(CODESET), "UCS-4BE");
 		    if (cd == (iconv_t)-1) {
 			zerr("cannot do charset conversion (iconv failed)");
