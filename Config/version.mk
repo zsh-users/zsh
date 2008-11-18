689df35c948b4c6e841fb278c5932bff4c0cee68
Peter Stephenson <pws@users.sourceforge.net>
2008-11-18 16:10:11 +0000

    26065: update version number because of syntax change

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=4.3.9-dev-0
-VERSION_DATE='November 3, 2008'
+VERSION=4.3.9-dev-1
+VERSION_DATE='November 18, 2008'
