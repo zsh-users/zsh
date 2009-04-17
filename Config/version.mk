71c30e6481c1fd5257e758ef2806c8e7b0be4ba4
Peter Stephenson <pws@users.sourceforge.net>
2009-04-17 15:13:20 +0000

    unposted: change version to 4.3.9-dev-2

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=4.3.9-dev-1
-VERSION_DATE='November 18, 2008'
+VERSION=4.3.9-dev-2
+VERSION_DATE='April 17, 2009'
