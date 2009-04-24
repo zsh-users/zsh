9ef573347a6eaca260001d84f50876a8cc08b2ca
Peter Stephenson <pws@users.sourceforge.net>
2009-04-24 09:06:17 +0000

    unposted: update to 4.3.9-dev-3

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=4.3.9-dev-2
-VERSION_DATE='April 17, 2009'
+VERSION=4.3.9-dev-3
+VERSION_DATE='April 24, 2009'
