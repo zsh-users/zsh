b3589121b3ff7b4c5e201bbca51d84d0cf90fd4b
Peter Stephenson <pws@users.sourceforge.net>
2010-12-21 09:35:33 +0000

    unposted: update version to 4.3.11-dev-1

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=4.3.11
-VERSION_DATE='December 20, 2010'
+VERSION=4.3.11-dev-1
+VERSION_DATE='December 21, 2010'
