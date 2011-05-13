23a057a4fad51caaf38a30c5611a5341718c7304
Peter Stephenson <pws@users.sourceforge.net>
2011-05-13 11:56:40 +0000

    unposted (late commit): updates for 4.3.11-dev-3

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=4.3.11-dev-2
-VERSION_DATE='April 19, 2011'
+VERSION=4.3.11-dev-3
+VERSION_DATE='May 12, 2011'
