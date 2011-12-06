79f2f7fa2ce18552869afa33f1d0d42cf4191b04
Peter Stephenson <pws@users.sourceforge.net>
2011-12-06 19:49:34 +0000

    unposted: 4.3.14

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=4.3.13
-VERSION_DATE='November 30, 2011'
+VERSION=4.3.14
+VERSION_DATE='December 6, 2011'
