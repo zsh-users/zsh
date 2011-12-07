b0182829307bff1b33579ecd18079342c0358109
Peter Stephenson <pws@users.sourceforge.net>
2011-12-07 20:13:39 +0000

    Update version to 4.3.14-dev-0

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=4.3.14
-VERSION_DATE='December 6, 2011'
+VERSION=4.3.14-dev-0
+VERSION_DATE='December 7, 2011'
