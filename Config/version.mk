fceb07892429f7871381cb208c297f601273ea64
Peter Stephenson <pws@users.sourceforge.net>
2011-12-19 11:12:28 +0000

    unposted: update version to 4.3.15-dev-0 to avoid clash with 4.3.15 install

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=4.3.15
-VERSION_DATE='December 17, 2011'
+VERSION=4.3.15-dev-0
+VERSION_DATE='December 19, 2011'
