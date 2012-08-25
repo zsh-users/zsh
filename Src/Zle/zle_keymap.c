9c49c8dd27cf0570703e39a11853f10bcf60fec1
Peter Stephenson <pws@users.sourceforge.net>
2012-08-25 18:02:32 +0000

    John Stahara: 30641: bindkey -N error report is misleading when copying keymap.

--- Src/Zle/zle_keymap.c
+++ Src/Zle/zle_keymap.c
@@ -912,7 +912,7 @@ bin_bindkey_new(char *name, UNUSED(char *kmname), Keymap km, char **argv, UNUSED
     if(argv[1]) {
 	km = openkeymap(argv[1]);
 	if(!km) {
-	    zwarnnam(name, "no such keymap `%s'", argv[0]);
+	    zwarnnam(name, "no such keymap `%s'", argv[1]);
 	    return 1;
 	}
     } else
