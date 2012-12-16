2908d73f6cc464d0e0c71745686b06f3f97d7542
Peter Stephenson <pws@users.sourceforge.net>
2012-12-16 18:39:55 +0000

    unposted: update for 5.0.0-test-1

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=5.0.0-dev-0
-VERSION_DATE='July 27, 2012'
+VERSION=5.0.0-test-1
+VERSION_DATE='December 16, 2012'
