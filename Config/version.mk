daa828bc51b48110f0a584fa2b74895d2cc761c1
Peter Stephenson <pws@users.sourceforge.net>
2012-12-21 19:35:07 +0000

    release 5.0.2

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=5.0.1
-VERSION_DATE='December 20, 2012'
+VERSION=5.0.2
+VERSION_DATE='December 21, 2012'
