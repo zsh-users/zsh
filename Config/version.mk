1873fd0684a4cce2b6eeb21b9d53490e91fb809c
Peter Stephenson <pws@users.sourceforge.net>
2013-01-03 13:34:53 +0000

    unposted: update version to 5.0.2-dev-0 to avoid clash with release

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=5.0.2
-VERSION_DATE='December 21, 2012'
+VERSION=5.0.2-dev-0
+VERSION_DATE='January 3, 2013'
