f9fdaff1613094822bb7a5b5e2d654316bb45afe
Peter Stephenson <p.w.stephenson@ntlworld.com>
2013-11-25 11:06:21 +0000

    unposted: Prepare 5.0.3-test-2.

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=5.0.2-test-1
-VERSION_DATE='November 6, 2013'
+VERSION=5.0.2-test-2
+VERSION_DATE='November 25, 2013'
