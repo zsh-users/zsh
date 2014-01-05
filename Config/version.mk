79abe00e1d73b3f0681343a70e3e7f84e5e3d647
Peter Stephenson <p.w.stephenson@ntlworld.com>
2014-01-05 18:24:59 +0000

    unposted: update version to 5.0.5 Also correct typo in compsys.yo for texinfo.

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=5.0.4-dev-0
-VERSION_DATE='December 21, 2013'
+VERSION=5.0.5
+VERSION_DATE='January 5, 2014'
