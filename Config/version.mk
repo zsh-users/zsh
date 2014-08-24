cc69eef1cd835cd8d3f33960e9f616c894e7ae4f
Peter Stephenson <p.w.stephenson@ntlworld.com>
2014-08-24 18:07:32 +0100

    unposted: 5.0.5-dev-3

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=5.0.5-dev-2
-VERSION_DATE='August 12, 2014'
+VERSION=5.0.5-dev-3
+VERSION_DATE='August 24, 2014'
