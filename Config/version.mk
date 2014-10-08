185476e70e4a9b33481be13f915c2b3c6f7a311d
Peter Stephenson <p.w.stephenson@ntlworld.com>
2014-10-08 19:55:33 +0100

    Update version number to 5.0.7-dev-0 to avoid installation clashes with 5.0.7

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=5.0.7
-VERSION_DATE='October 7, 2014'
+VERSION=5.0.7-dev-0
+VERSION_DATE='October 8, 2014'
