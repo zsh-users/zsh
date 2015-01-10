81ad9ed76634173f9563eb155cf00c5e25169c5a
Peter Stephenson <p.w.stephenson@ntlworld.com>
2015-01-10 19:55:30 +0000

    unposted: update copyright for new Src/context.c

--- Src/context.c
+++ Src/context.c
@@ -3,7 +3,7 @@
  *
  * This file is part of zsh, the Z shell.
  *
- * Copyright (c) 1992-1997 Paul Falstad
+ * Copyright (c) 2015 Peter Stephenson
  * All rights reserved.
  *
  * Permission is hereby granted, without written agreement and without
@@ -12,16 +12,16 @@
  * purpose, provided that the above copyright notice and the following
  * two paragraphs appear in all copies of this software.
  *
- * In no event shall Paul Falstad or the Zsh Development Group be liable
+ * In no event shall Peter Stephenson or the Zsh Development Group be liable
  * to any party for direct, indirect, special, incidental, or consequential
  * damages arising out of the use of this software and its documentation,
- * even if Paul Falstad and the Zsh Development Group have been advised of
+ * even if Peter Stephenson and the Zsh Development Group have been advised of
  * the possibility of such damage.
  *
- * Paul Falstad and the Zsh Development Group specifically disclaim any
+ * Peter Stephenson and the Zsh Development Group specifically disclaim any
  * warranties, including, but not limited to, the implied warranties of
  * merchantability and fitness for a particular purpose.  The software
- * provided hereunder is on an "as is" basis, and Paul Falstad and the
+ * provided hereunder is on an "as is" basis, and Peter Stephenson and the
  * Zsh Development Group have no obligation to provide maintenance,
  * support, updates, enhancements, or modifications.
  *
