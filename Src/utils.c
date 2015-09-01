cbe00216fe2b61a4d00cd6bd8189063efc5e132a
Mikael Magnusson <mikachu@gmail.com>
2015-09-01 23:14:37 +0200

    36380: Avoid gcc case syntax

--- Src/utils.c
+++ Src/utils.c
@@ -3088,7 +3088,8 @@ morefmt:
 	    case '#':
 	    case '_':
 	    case '-':
-	    case '0' ... '9':
+	    case '0': case '1': case '2': case '3': case '4':
+	    case '5': case '6': case '7': case '8': case '9':
 		goto morefmt;
 strftimehandling:
 	    default:
