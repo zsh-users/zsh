b73e4442e850a565525b7ed75f2a7b01698499d6
Peter Stephenson <pws@zsh.org>
2015-09-01 09:58:43 +0100

    unposted: Update internal version to 5.1-dev-0.
    Avoids installation clash of develpment build with 5.1 release.

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=5.1
-VERSION_DATE='August 30, 2015'
+VERSION=5.1-dev-0
+VERSION_DATE='September 1, 2015'
