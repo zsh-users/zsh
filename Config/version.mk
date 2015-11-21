cce4261a3c6f4bf78b483db61623c80e3c98d10b
Peter Stephenson <p.w.stephenson@ntlworld.com>
2015-11-21 18:09:27 +0000

    unposted: Make 5.1.1-test-1 in preparation for 5.2

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=5.1.1-dev-0
-VERSION_DATE='September 11, 2015'
+VERSION=5.1.1-test-1
+VERSION_DATE='November 21, 2015'
