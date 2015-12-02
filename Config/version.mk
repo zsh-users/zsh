c86c20ae577312b01a2a1c13c312b4b5b21fe990
Peter Stephenson <p.w.stephenson@ntlworld.com>
2015-12-02 18:55:11 +0000

    unposted: zsh 5.2

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=5.1.1-test-3
-VERSION_DATE='November 29, 2015'
+VERSION=5.2
+VERSION_DATE='December 2, 2015'
