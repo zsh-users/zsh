2ec61f3a0bc3ea100b6601821ae0a44c0063b776
Peter Stephenson <pws@zsh.org>
2015-12-03 12:38:31 +0000

    unposted: Update internal version to 5.2-dev-0.
    Also add entry for 5.2 release in ChangeLog.

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=5.2
-VERSION_DATE='December 2, 2015'
+VERSION=5.2-dev-0
+VERSION_DATE='December 3, 2015'
