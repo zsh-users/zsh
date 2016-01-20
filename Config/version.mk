d18ce35d060163ea0d531170b65e1c38311af166
Peter Stephenson <pws@zsh.org>
2016-01-20 09:36:25 +0000

    unposted: increment version to 5.2-dev-1

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=5.2-dev-0
-VERSION_DATE='December 3, 2015'
+VERSION=5.2-dev-1
+VERSION_DATE='January 20, 2015'
