ccc0b6f8a625357cbae9dfd4a685b9323e09c13f
Barton E. Schaefer <schaefer@zsh.org>
2016-10-03 09:40:39 -0700

    39550: reset signal queue in recursiveedit()

--- Src/Zle/zle_main.c
+++ Src/Zle/zle_main.c
@@ -1868,10 +1868,16 @@ int
 recursiveedit(UNUSED(char **args))
 {
     int locerror;
+    int q = queue_signal_level();
+
+    /* zlecore() expects to be entered with signal queue disabled */
+    dont_queue_signals();
 
     redrawhook();
     zrefresh();
     zlecore();
+
+    restore_queue_signals(q);
 
     locerror = errflag ? 1 : 0;
     errflag = done = eofsent = 0;
