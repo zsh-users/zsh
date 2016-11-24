3a8727dc6bae5fd762256cbccdde55b75bed8769
Daniel Shahaf <d.s@daniel.shahaf.name>
2016-11-24 06:57:51 +0000

    40009: alias -L: Emit aliases that begin with a plus sign correctly.

--- Src/hashtable.c
+++ Src/hashtable.c
@@ -1291,9 +1291,9 @@ printaliasnode(HashNode hn, int printflags)
 	else if (a->node.flags & ALIAS_GLOBAL)
 	    printf("-g ");
 
-	/* If an alias begins with `-', then we must output `-- ' *
+	/* If an alias begins with `-' or `+', then we must output `-- '
 	 * first, so that it is not interpreted as an option.     */
-	if(a->node.nam[0] == '-')
+	if(a->node.nam[0] == '-' || a->node.nam[0] == '+')
 	    printf("-- ");
     }
 
