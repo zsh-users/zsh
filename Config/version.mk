4cfdbdb877fe056f71eba8514180c2007443ee94
Peter Stephenson <p.w.stephenson@ntlworld.com>
2016-12-11 16:52:20 +0000

    unposted: Release 5.3

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=5.2-test-3
-VERSION_DATE='December 9, 2016'
+VERSION=5.3
+VERSION_DATE='December 11, 2016'
