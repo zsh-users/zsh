5f2661376d686401a42aa15e5e3cbb6ec86a8f6c
Peter Stephenson <p.w.stephenson@ntlworld.com>
2016-12-12 21:37:16 +0000

    unposted: update version to 5.3-dev-0.
    Avoid installed version trashing 5.3 release.

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=5.3
-VERSION_DATE='December 11, 2016'
+VERSION=5.3-dev-0
+VERSION_DATE='December 12, 2016'
