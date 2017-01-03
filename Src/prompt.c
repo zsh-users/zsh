8d4c98540de9bcdba8565facc91fbc45855d27e2
Paulo Andrade <pandrade@redhat.com>
2017-01-03 12:29:34 +0100

    40260: zero new space allocated in prompt buffer

--- Src/prompt.c
+++ Src/prompt.c
@@ -920,6 +920,7 @@ addbufspc(int need)
 	if(need & 255)
 	    need = (need | 255) + 1;
 	bv->buf = realloc(bv->buf, bv->bufspc += need);
+	memset(bv->buf + bv->bufspc - need, 0, need);
 	bv->bp = bv->buf + bo;
 	if(bo1 != -1)
 	    bv->bp1 = bv->buf + bo1;
