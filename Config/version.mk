d17f9d32f0e65c3956193be06130dd7cec275c9a
Peter Stephenson <p.w.stephenson@ntlworld.com>
2017-07-31 21:14:50 +0100

    unpostes: test release 5.3.1-test-2

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=5.3.1-dev-1
-VERSION_DATE='December 22, 2016'
+VERSION=5.3.1-test-2
+VERSION_DATE='July 31, 2017'
