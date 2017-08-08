36185563f8fd263cf5138c69c0edd17c33cda1d2
Daniel Shahaf <d.s@daniel.shahaf.name>
2017-08-08 13:51:43 +0000

    unposted: Post-release version number bump to 5.4-dev-0.

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=5.4
-VERSION_DATE='August 7, 2017'
+VERSION=5.4-dev-0
+VERSION_DATE='August 8, 2017'
