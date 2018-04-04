f027f1d6e876708bc75d4217e1ca26898658d8d3
Peter Stephenson <p.w.stephenson@ntlworld.com>
2018-04-04 18:34:08 +0100

    unposted: 5.4.2-test-2

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=5.4.2-dev-0
-VERSION_DATE='August 28, 2017'
+VERSION=5.4.2-test-2
+VERSION_DATE='April 4, 2018'
