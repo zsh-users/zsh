f9e9dce5443f323b340303596406f9d3ce11d23a
Peter Stephenson <p.w.stephenson@ntlworld.com>
2018-04-08 16:52:30 +0100

    unposted: release zsh-5.5

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=5.4.2-test-2
-VERSION_DATE='April 4, 2018'
+VERSION=5.5
+VERSION_DATE='April 8, 2018'
