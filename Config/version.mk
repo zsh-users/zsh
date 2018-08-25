17c0439936087bdf79746d583f8f58ad6c61d86f
Daniel Shahaf <d.s@daniel.shahaf.name>
2018-08-25 17:56:35 +0000

    unposted: Test release: 5.5.1-test-1.

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=5.5.1-dev-0
-VERSION_DATE='April 17, 2018'
+VERSION=5.5.1-test-1
+VERSION_DATE='August 25, 2018'
