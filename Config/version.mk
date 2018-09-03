b30b89418af2495c0d48a72573f908c4ecf05efd
Daniel Shahaf <d.s@daniel.shahaf.name>
2018-09-03 15:09:46 +0000

    unposted: Release 5.6.

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=5.5.1-test-2
-VERSION_DATE='August 26, 2018'
+VERSION=5.6
+VERSION_DATE='September 3, 2018'
