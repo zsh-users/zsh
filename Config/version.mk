b3239c5e40eea0a73992160c5619b7d191ede490
Daniel Shahaf <d.s@daniel.shahaf.name>
2018-09-08 17:39:19 +0000

    unposted: Patch release: 5.6.1

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=5.6-dev-0
-VERSION_DATE='September 4, 2018'
+VERSION=5.6.1
+VERSION_DATE='September 8, 2018'
