9dbde9e9c7d22ee0d301e4a2fecf97906d1ddce9
Peter Stephenson <p.w.stephenson@ntlworld.com>
2018-12-19 19:33:36 +0000

    unposted: updates for 5.6.2-test-2

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=5.6.2-dev-1
-VERSION_DATE='October 11, 2018'
+VERSION=5.6.2-test-2
+VERSION_DATE='December 19, 2018'
