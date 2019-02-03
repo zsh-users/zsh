8b89d0d57710d044e85591aac25952f5a6ae84c7
dana <dana@dana.is>
2019-02-03 13:27:45 -0600

    unposted: Release 5.7.1

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=5.7-dev-0
-VERSION_DATE='January 25, 2019'
+VERSION=5.7.1
+VERSION_DATE='February 3, 2019'
