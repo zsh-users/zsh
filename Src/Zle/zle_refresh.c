zsh-users/zsh

commit 3eea35d0853bddae13fa6f122669935a01618bf9

fix cursor position with ZLE_RPROMPT_INDENT=0

Src/Zle/zle_refresh.c
@@ -1678,7 +1678,12 @@ zrefresh(void)
 
 	    moveto(0, winw - rprompt_off - rpromptw);
 	    zputs(rpromptbuf, shout);
-	    vcs = winw - rprompt_off;
+	    if (rprompt_off) {
+		vcs = winw - rprompt_off;
+	    } else {
+		zputc(&zr_cr);
+		vcs = 0;
+	    }
 	/* reset character attributes to that set by the main prompt */
 	    txtchange = pmpt_attr;
 	    /*
@@ -2159,25 +2164,20 @@ moveto(int ln, int cl)
     const REFRESH_ELEMENT *rep;
 
     if (vcs == winw) {
-	if (rprompt_indent == 0 && tccan(TCLEFT)) {
-	  tc_leftcurs(1);
-	  vcs--;
-	} else {
-	    vln++, vcs = 0;
-	    if (!hasam) {
-		zputc(&zr_cr);
-		zputc(&zr_nl);
-	    } else {
-		if ((vln < nlnct) && nbuf[vln] && nbuf[vln]->chr)
-		    rep = nbuf[vln];
-		else
-		    rep = &zr_sp;
-		zputc(rep);
-		zputc(&zr_cr);
-		if ((vln < olnct) && obuf[vln] && obuf[vln]->chr)
-		    *obuf[vln] = *rep;
-	    }
-	}
+    vln++, vcs = 0;
+    if (!hasam) {
+	zputc(&zr_cr);
+	zputc(&zr_nl);
+    } else {
+	if ((vln < nlnct) && nbuf[vln] && nbuf[vln]->chr)
+	    rep = nbuf[vln];
+	else
+	    rep = &zr_sp;
+	zputc(rep);
+	zputc(&zr_cr);
+	if ((vln < olnct) && obuf[vln] && obuf[vln]->chr)
+	    *obuf[vln] = *rep;
+    }
     }
 
     if (ln == vln && cl == vcs)

commit 6c6200520afa0409c5c310cd421f33353c44c61d

unposted: fix indentation (whitespace-only change)
Indentatin in moveto was broken by 3eea35d0 (my bad), making it
difficult to read. This commit reverts it to what it used ot be.
While at it, fix indentation in a few other places in the same
file.

Src/Zle/zle_refresh.c
@@ -834,7 +834,7 @@ nextline(Rparams rpms, int wrapped)
 	    if (rpms->nvln != -1 && rpms->nvln != winh - 1
 		&& (numscrolls != onumscrolls - 1
 		    || rpms->nvln <= winh / 2))
-	        return 1;
+		return 1;
 	    numscrolls++;
 	    rpms->canscroll = winh / 2;
 	}
@@ -1151,20 +1151,20 @@ zrefresh(void)
 	resetneeded = 0;	/* unset */
 	oput_rpmpt = 0;		/* no right-prompt currently on screen */
 
-        if (!clearflag) {
-            if (tccan(TCCLEAREOD))
-                tcoutclear(TCCLEAREOD);
-            else
-                cleareol = 1;   /* request: clear to end of line */
+	if (!clearflag) {
+	    if (tccan(TCCLEAREOD))
+		tcoutclear(TCCLEAREOD);
+	    else
+		cleareol = 1;   /* request: clear to end of line */
 	    if (listshown > 0)
 		listshown = 0;
 	}
-        if (t0 > -1)
-            olnct = (t0 < winh) ? t0 : winh;
-        if (termflags & TERM_SHORT)
-            vcs = 0;
+	if (t0 > -1)
+	    olnct = (t0 < winh) ? t0 : winh;
+	if (termflags & TERM_SHORT)
+	    vcs = 0;
 	else if (!clearflag && lpromptbuf[0]) {
-            zputs(lpromptbuf, shout);
+	    zputs(lpromptbuf, shout);
 	    if (lpromptwof == winw)
 		zputs("\n", shout);	/* works with both hasam and !hasam */
 	} else {
@@ -1890,7 +1890,7 @@ refreshline(int ln)
     if (hasam && vcs == winw) {
 	if (nbuf[vln] && nbuf[vln][vcs + 1].chr == ZWC('\n')) {
 	    vln++, vcs = 1;
-            if (nbuf[vln]  && nbuf[vln]->chr) {
+	    if (nbuf[vln]  && nbuf[vln]->chr) {
 		zputc(nbuf[vln]);
 	    } else
 		zputc(&zr_sp);  /* I don't think this should happen */
@@ -1959,11 +1959,11 @@ refreshline(int ln)
 	    if (!nl->chr) {
 		if (ccs == winw && hasam && char_ins > 0 && ins_last
 		    && vcs != winw) {
-		    nl--;           /* we can assume we can go back here */
+		    nl--;	   /* we can assume we can go back here */
 		    moveto(ln, winw - 1);
 		    zputc(nl);
 		    vcs++;
-		    return;         /* write last character in line */
+		    return;	 /* write last character in line */
 		}
 		if ((char_ins <= 0) || (ccs >= winw))    /* written everything */
 		    return;
@@ -2164,20 +2164,20 @@ moveto(int ln, int cl)
     const REFRESH_ELEMENT *rep;
 
     if (vcs == winw) {
-    vln++, vcs = 0;
-    if (!hasam) {
-	zputc(&zr_cr);
-	zputc(&zr_nl);
-    } else {
-	if ((vln < nlnct) && nbuf[vln] && nbuf[vln]->chr)
-	    rep = nbuf[vln];
-	else
-	    rep = &zr_sp;
-	zputc(rep);
-	zputc(&zr_cr);
-	if ((vln < olnct) && obuf[vln] && obuf[vln]->chr)
-	    *obuf[vln] = *rep;
-    }
+	vln++, vcs = 0;
+	if (!hasam) {
+	    zputc(&zr_cr);
+	    zputc(&zr_nl);
+	} else {
+	    if ((vln < nlnct) && nbuf[vln] && nbuf[vln]->chr)
+		rep = nbuf[vln];
+	    else
+		rep = &zr_sp;
+	    zputc(rep);
+	    zputc(&zr_cr);
+	    if ((vln < olnct) && obuf[vln] && obuf[vln]->chr)
+		*obuf[vln] = *rep;
+	}
     }
 
     if (ln == vln && cl == vcs)
@@ -2212,7 +2212,7 @@ moveto(int ln, int cl)
     }
 
     if (cl != vcs)
-        singmoveto(cl);
+	singmoveto(cl);
 }
 
 /**/
@@ -2292,7 +2292,7 @@ tc_rightcurs(int ct)
 	    /* it is cheaper to send TCRIGHT than reprint the whole prompt */
 	    for (ct = lpromptw - i; ct--; )
 		tcout(TCRIGHT);
-        else {
+	else {
 	    if (i != 0)
 		zputc(&zr_cr);
 	    tc_upcurs(lprompth - 1);