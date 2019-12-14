5ed3dde0d220dcc6d8503a644a50e661d877bf2c
dana <dana@dana.is>
2019-12-14 12:50:58 -0600

    unposted: Test release: 5.7.1-test-1

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=5.7.1-dev-0
-VERSION_DATE='February 4, 2019'
+VERSION=5.7.1-test-1
+VERSION_DATE='December 14, 2019'
