77d203f3fbbd76386bf197f9776269a1de580bb5
dana <dana@dana.is>
2020-02-14 11:30:56 -0600

    unposted: Release 5.8

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=5.7.1-test-3
-VERSION_DATE='February 6, 2020'
+VERSION=5.8
+VERSION_DATE='February 14, 2020'
