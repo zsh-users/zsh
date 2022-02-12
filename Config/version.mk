1a490c7050cfacd5daf8376d5a1f0e8cb14ccda5
dana <dana@dana.is>
2022-02-12 06:41:26 -0600

    unposted: Release 5.8.1

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=5.8
-VERSION_DATE='February 14, 2020'
+VERSION=5.8.1
+VERSION_DATE='February 12, 2022'
