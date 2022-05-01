f72ce8843ecdba3e8a2c5604a44053873948f09f
dana <dana@dana.is>
2022-05-01 04:58:54 -0500

    unposted: Test release: 5.8.1.3-test

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=5.8.1.2-test
-VERSION_DATE='April 9, 2022'
+VERSION=5.8.1.3-test
+VERSION_DATE='May 1, 2022'
