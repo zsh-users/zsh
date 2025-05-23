f90dbf36749af04aa8f946d5922feacd47cb0fa5
dana <dana@dana.is>
2025-05-23 04:20:06 -0500

    53671: hist: untokenise input to :A and :P

--- Src/hist.c
+++ Src/hist.c
@@ -1999,6 +1999,7 @@ chrealpath(char **junkptr, char mode, int use_heap)
     if (**junkptr != '/')
 	return 0;
 
+    untokenize(*junkptr);
     unmetafy(*junkptr, NULL);
 
     lastpos = strend(*junkptr);
