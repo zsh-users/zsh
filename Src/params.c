bcae4b58e6539423a5168afa0b651cfdab8b3cc3
Philippe Altherr <philippe.altherr@gmail.com>
2026-03-31 17:55:43 -0700

    54048: consistently re-bind namerefs when returning from greater locallevel

--- Src/params.c
+++ Src/params.c
@@ -5905,7 +5905,8 @@ endparamscope(void)
     for (Param pm; refs && (pm = (Param)getlinknode(refs));) {
 	if ((pm->node.flags & PM_NAMEREF) && !(pm->node.flags & PM_UNSET) &&
 	    !(pm->node.flags & PM_UPPER) && pm->base > locallevel) {
-	    setscope_base(pm, locallevel);
+	    pm->base = 0;
+	    setscope(pm);
 	}
     }
     unqueue_signals();
