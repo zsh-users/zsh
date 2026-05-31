460c74e0cfe80d71339c0c5cdcc5200248b0a3a3
dana <dana@dana.is>
2026-05-31 15:15:49 -0500

    unposted: post-release version bump

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=5.9.1
-VERSION_DATE='May 31, 2026'
+VERSION=5.9.1.1-dev
+VERSION_DATE='June 1, 2026'
