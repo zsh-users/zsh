95a758d2589da9cbdc6a56ac53edb8dba9d6761c
dana <dana@dana.is>
2026-06-28 15:05:15 -0500

    unposted: test release: 5.9.1.2-test

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=5.9.1.1-dev
-VERSION_DATE='June 1, 2026'
+VERSION=5.9.1.2-test
+VERSION_DATE='June 28, 2026'
